8b883bf0fcdb6850dcafa2709b082f1eafa8ca93
Erick Ramirez <erickramirez.dnbk@gmail.com>
2023-06-13 09:29:31 -0400

    migración de tratamiento terminada

--- database/migrations/2023_06_12_234130_create_treatments_table.php
+++ database/migrations/2023_06_12_234130_create_treatments_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('treatments', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->decimal('price', 5, 2);
             $table->timestamps();
         });
     }
