34b58be3fa9721e18954d7b129de6b4cbbd74fc4
Erick Ramirez <erickramirez.dnbk@gmail.com>
2023-06-13 09:18:19 -0400

    migración, modelo, requests y controlador de tratamiento creados

---
+++ app/Http/Controllers/Admin/TreatmentController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Treatment;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTreatmentRequest;
+use App\Http\Requests\UpdateTreatmentRequest;
+
+class TreatmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTreatmentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Treatment $treatment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Treatment $treatment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTreatmentRequest $request, Treatment $treatment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Treatment $treatment)
+    {
+        //
+    }
+}
